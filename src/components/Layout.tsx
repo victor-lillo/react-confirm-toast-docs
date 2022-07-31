068f2d6c169d86d45967dbfedb55f8042ce6f4f1
fentosv <victortic0910@gmail.com>
2022-07-31 19:14:14 +0200

    CSS

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,16 @@
 import { ReactNode } from 'react';
 
-import Navbar from '@components/Navbar';
 import Footer from '@components/Footer';
-
 
 interface Props {
     children: ReactNode;
-    navbarClass?: string;
-    bodyClass?: string;
-    contentClass?: string;
-    footerClass?: string;
 }
 
-export default function Layout({ children, navbarClass, footerClass }: Props) {
+export default function Layout({ children, }: Props) {
     return (
         <>
-            {/* <Navbar navbarClass={navbarClass} /> */}
             <main className='body'>{children}</main>
-            <Footer footerClass={footerClass} />
+            <Footer />
         </>
     )
 }
