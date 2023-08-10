d9c83a9db25262d1b22db8071531cf2bc21b39a2
fentosv <fentos.dev@gmail.com>
2023-08-10 09:24:34 +0200

    Remove comments

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -1,32 +1,20 @@
-import React, { useEffect } from "react";
-import Prism from "prismjs";
-
-// import "prismjs/themes/prism-coy.css";
-// import "prismjs/themes/prism-dark.css";
-// import "prismjs/themes/prism-okaidia.css";
-// import "prismjs/themes/prism-solarizedlight.css";
-// import "prismjs/themes/prism-tomorrow.css";
-// import "prismjs/themes/prism-twilight.css";
+import React, { useEffect } from 'react'
+import Prism from 'prismjs'
 
 interface Props {
-    children: string
-    language?: string
+  children: string
+  language?: string
 }
 
 function Code({ language = 'jsx', children }: Props) {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [children])
 
-    useEffect(() => {
-        Prism.highlightAll();
-    }, [children]);
-
-    return (
-        <pre>
-            <code
-                className={`language-${language}`}
-            >
-                {children}
-            </code>
-        </pre>
-    );
+  return (
+    <pre>
+      <code className={`language-${language}`}>{children}</code>
+    </pre>
+  )
 }
-export default Code;
+export default Code
